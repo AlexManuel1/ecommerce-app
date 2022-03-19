db53c477e5d93c38bf75102d9d63d7360d538467
AlexManuel1 <AlexMatamoros10@gmail.com>
2022-03-18 23:36:54 -0700

    initial commit

---
+++ client/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: 'https://images.unsplash.com/photo-1576193929674-c1da148d6bdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
+        title: 'NEW ITEMS FOR SUMMER',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+        bg: 'f5fafd'
+    },
+    {
+        id: 2,
+        img: 'https://images.unsplash.com/photo-1559582798-678dfc71ccd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
+        title: 'NEW ITEMS FOR WINTER',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+        bg: 'fcf1ed'  
+    },
+    {
+        id: 3,
+        img: 'https://images.unsplash.com/photo-1526662511954-7198d9018087?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
+        title: 'SHOP POPULAR ITEMS',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+        bg: 'fbf0f4'  
+    }
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: 'https://images.unsplash.com/photo-1552327359-d86398116072?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGphY2tldHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+        title: 'JACKETS'
+    },
+    {
+        id: 2,
+        img: 'https://images.unsplash.com/photo-1526476148966-98bd039463ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
+        title: 'HOODIES'
+    },
+    {
+        id: 3,
+        img: 'https://images.unsplash.com/photo-1627910087352-f4e3e28d9f5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        title: 'T-SHIRTS'
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: 'https://cdn.shopify.com/s/files/1/0296/0845/products/JACKET_HO21_CORDUROYWAVEPUFFER_LIGHTBLUE-01_1024x1024.jpg?v=1637675015'
+    },
+    {
+        id: 2,
+        img: 'https://media.endclothing.com/media/f_auto,q_auto:eco/prodmedia/media/catalog/product/1/7/17-03-2022_SI_SS2204314-NBD_1_1.jpg'
+    },
+    {
+        id: 3,
+        img: 'https://media.endclothing.com/media/f_auto,q_auto:eco/prodmedia/media/catalog/product/2/8/28-01-2022_RC_I009578-8902_1_1.jpg'
+    },
+    {
+        id: 4,
+        img: 'https://media.endclothing.com/media/f_auto,q_auto:eco/prodmedia/media/catalog/product/1/2/12-07-2021_GH_4154M-63_1_1.jpg'
+    },
+    {
+        id: 5,
+        img: 'https://media.endclothing.com/media/f_auto,q_auto:eco/prodmedia/media/catalog/product/2/3/23-03-2021_ML_1116-0888-139-79_1_1.jpg'
+    },
+    {
+        id: 6,
+        img: 'https://media.endclothing.com/media/f_auto,q_auto:eco/prodmedia/media/catalog/product/1/8/18-03-2022_GH_001LJH701007J-BLK_1_1.jpg'
+    },
+    {
+        id: 7,
+        img: 'https://media.endclothing.com/media/f_auto,q_auto:eco/prodmedia/media/catalog/product/1/7/17-03-2022_ML_UE-220032-IND_1_1.jpg'
+    },
+    {
+        id: 8,
+        img: 'https://media.endclothing.com/media/f_auto,q_auto:eco/prodmedia/media/catalog/product/1/7/17-03-2022_SI_NF0A5IX5537_1_1.jpg'
+    }
+]
